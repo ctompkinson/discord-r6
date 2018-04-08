5f7fafaca61646dc7134e45b4250ee7f281696b4
Chris Tompkinson <ctompkinson@gmail.com>
2018-04-08 19:44:17 +0100

    Add translation of op abilities

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 	}
 
 	// Register the messageCreate func as a callback for MessageCreate events.
-	dg.AddHandler(printStats)
+	dg.AddHandler(getMessageHandler)
 
 	// Open a websocket connection to Discord and begin listening.
 	err = dg.Open()
@@ -55,17 +55,22 @@ func main() {
 
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the autenticated bot has access to.
-func printStats(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if !strings.Contains(m.Message.Content, "!stats ") {
+func getMessageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// If we didn't receive an /r6 command then we don't need to continue
+	if !strings.Contains(m.Message.Content, "/r6") {
 		return
 	}
 
+	// split the message up for processing
 	parts := strings.Split(m.Message.Content, " ")
+
 	if len(parts) == 1 {
-		s.ChannelMessageSendEmbed(m.ChannelID, newMsg("No player specified"))
+		s.ChannelMessageEditEmbed(m.ChannelID, m.ID, newMsg("No player specified"))
 		return
 	}
 
+	// Send the initial loading message
 	fmt.Println("Sending stats")
 	loadingMsg := discordgo.MessageEmbed{
 		Title: "Loading stats for " + parts[1],
@@ -76,42 +81,31 @@ func printStats(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	//getOperators := len(parts) == 3
+	// Create the r6stats client and fetch the player
 	r6 := r6.NewClient(http.Client{})
 	player, err := r6.GetPlayer(parts[1], "uplay", true)
 	if err != nil {
-		s.ChannelMessageSendEmbed(m.ChannelID, newMsg("Unable to find player"))
+		_, err = s.ChannelMessageEditEmbed(m.ChannelID, m.Message.ID, &discordgo.MessageEmbed{
+			Title: "Failed to load player",
+		})
 		return
 	}
 
-	if len(parts) == 3 {
-		sendOperatorMessage(s, m, player, strings.TrimSpace(parts[2]))
-		return
+	if len(parts) == 2 {
+		// /r6 ruffbabe
+		// Print the player stats
+		sendStatsMessage(s, m, player, msg)
+	} else if len(parts) == 3 {
+		// /r6 ruffbabe fuze
+		// Print the player operator stats
+		sendOperatorMessage(s, m, player, strings.TrimSpace(parts[2]), msg)
 	}
 
-	sendStatsMessage(s, m, player, msg)
 	return
 }
 
 func sendStatsMessage(s *discordgo.Session, m *discordgo.MessageCreate, player r6.Player, msg *discordgo.Message) {
-
-	// Some intial operators to compare against
-	favOff := player.Operators["ash"]
-	favDef := player.Operators["frost"]
-
-	for _, op := range player.Operators {
-		if op.Role == "atk" {
-			if op.Playtime > favOff.Playtime {
-				favOff = op
-			}
-		}
-
-		if op.Role == "def" {
-			if op.Playtime > favDef.Playtime {
-				favDef = op
-			}
-		}
-	}
+	favOff, favDef := favOps(player)
 
 	favOffKD := operatorKD(&favOff)
 	favOffWL := operatorWL(&favOff)
@@ -126,7 +120,7 @@ func sendStatsMessage(s *discordgo.Session, m *discordgo.MessageCreate, player r
 			// Ranked
 			{
 				Name:   "Ranked PlayTime",
-				Value:  strconv.FormatFloat(player.Stats.Ranked.Playtime / 60 / 60, 'f', 1, 64),
+				Value:  strconv.FormatFloat(player.Stats.Ranked.Playtime/60/60, 'f', 1, 64),
 				Inline: true,
 			},
 			{
@@ -143,7 +137,7 @@ func sendStatsMessage(s *discordgo.Session, m *discordgo.MessageCreate, player r
 			// Casual
 			{
 				Name:   "Casual PlayTime",
-				Value:  strconv.FormatFloat(player.Stats.Casual.Playtime / 60 / 60, 'f', 1, 64),
+				Value:  strconv.FormatFloat(player.Stats.Casual.Playtime/60/60, 'f', 1, 64),
 				Inline: true,
 			},
 			{
@@ -159,35 +153,35 @@ func sendStatsMessage(s *discordgo.Session, m *discordgo.MessageCreate, player r
 
 			// Offensive Operator
 			{
-				Name: "Favorite Atk",
-				Value: favOff.Name,
+				Name:   "Favorite Atk",
+				Value:  favOff.Name,
 				Inline: true,
 			},
 			{
-				Name: "Kill/Death",
-				Value: strconv.FormatFloat(favOffKD, 'f', 3, 64),
+				Name:   "Kill/Death",
+				Value:  strconv.FormatFloat(favOffKD, 'f', 3, 64),
 				Inline: true,
 			},
 			{
 				Name:   "Win/Loss",
-				Value: strconv.FormatFloat(favOffWL, 'f', 3, 64),
+				Value:  strconv.FormatFloat(favOffWL, 'f', 3, 64),
 				Inline: true,
 			},
 
 			// Defensive Operator
 			{
-				Name: "Favorite Def",
-				Value: favDef.Name,
+				Name:   "Favorite Def",
+				Value:  favDef.Name,
 				Inline: true,
 			},
 			{
-				Name: "Kill/Death",
-				Value: strconv.FormatFloat(favDefKD, 'f', 3, 64),
+				Name:   "Kill/Death",
+				Value:  strconv.FormatFloat(favDefKD, 'f', 3, 64),
 				Inline: true,
 			},
 			{
 				Name:   "Win/Loss",
-				Value: strconv.FormatFloat(favDefWL, 'f', 3, 64),
+				Value:  strconv.FormatFloat(favDefWL, 'f', 3, 64),
 				Inline: true,
 			},
 		},
@@ -196,45 +190,61 @@ func sendStatsMessage(s *discordgo.Session, m *discordgo.MessageCreate, player r
 	s.ChannelMessageEditEmbed(m.ChannelID, msg.ID, &statsMsg)
 }
 
-func sendOperatorMessage(s *discordgo.Session, m *discordgo.MessageCreate, player r6.Player, operator string) {
-	msg := discordgo.MessageEmbed{
-		Title: fmt.Sprintf("Siege Stats for %s", player.Username),
-		Color: 10,
-		Fields: []*discordgo.MessageEmbedField{
-			{
-				Name:   "Ranked K/D",
-				Value:  strconv.FormatFloat(player.Stats.Ranked.KillDeathRatio, 'f', 3, 64),
-				Inline: true,
-			},
-			{
-				Name:   "Ranked W/L",
-				Value:  strconv.FormatFloat(player.Stats.Ranked.WinLossRatio, 'f', 3, 64),
-				Inline: true,
-			},
-			{
-				Name:   "Ranked PlayTime",
-				Value:  strconv.FormatFloat(player.Stats.Ranked.Playtime / 60 / 60, 'f', 1, 64),
-				Inline: true,
-			},
-			{
-				Name:   "Casual K/D",
-				Value:  strconv.FormatFloat(player.Stats.Casual.KillDeathRatio, 'f', 3, 64),
-				Inline: true,
-			},
-			{
-				Name:   "Casual W/L",
-				Value:  strconv.FormatFloat(player.Stats.Casual.WinLossRatio, 'f', 3, 64),
-				Inline: true,
-			},
-			{
-				Name:   "Casual PlayTime",
-				Value:  strconv.FormatFloat(player.Stats.Casual.Playtime / 60 / 60, 'f', 1, 64),
-				Inline: true,
-			},
+func sendOperatorMessage(s *discordgo.Session, m *discordgo.MessageCreate, player r6.Player, operatorName string, msg *discordgo.Message) {
+	fmt.Println("getting stats for " + operatorName)
+
+	opName := strings.Title(operatorName)
+
+	if _, ok := player.Operators[opName]; !ok {
+		_, err := s.ChannelMessageEditEmbed(msg.ChannelID, msg.ID, &discordgo.MessageEmbed{
+			Title: "Invalid operator name",
+		})
+		if err != nil {
+			log.Println(err)
+		}
+		return
+	}
+
+	op := player.Operators[opName]
+
+	// Generate the fields
+	fields := []*discordgo.MessageEmbedField{
+		{
+			Name:   "PlayTime",
+			Value:  strconv.FormatFloat(float64(op.Playtime)/60/60, 'f', 1, 64),
+			Inline: true,
+		},
+		{
+			Name:
+			"Kill/Death",
+			Value:  strconv.FormatFloat(operatorKD(&op), 'f', 3, 64),
+			Inline: true,
+		},
+		{
+			Name:   "Win/Loss",
+			Value:  strconv.FormatFloat(operatorWL(&op), 'f', 3, 64),
+			Inline: true,
 		},
 	}
 
-	s.ChannelMessageSendEmbed(m.ChannelID, &msg)
+	fmt.Println(op.Specials)
+	for k, v := range op.Specials {
+		fmt.Println(k)
+		fmt.Println(v)
+		fields = append(fields, &discordgo.MessageEmbedField{
+			Name:   k,
+			Value:  v,
+			Inline: true,
+		})
+	}
+
+	opMsg := discordgo.MessageEmbed{
+		Title: fmt.Sprintf("Siege Stats for %s: %s", player.Username, op.Name),
+		Color: 10,
+		Fields: fields,
+	}
+
+	s.ChannelMessageEditEmbed(m.ChannelID, msg.ID, &opMsg)
 }
 
 func operatorKD(op *r6.Operator) float64 {
@@ -243,6 +253,28 @@ func operatorKD(op *r6.Operator) float64 {
 
 func operatorWL(op *r6.Operator) float64 {
 	return float64(op.Wins) / float64(op.Losses)
+}
+
+func favOps(player r6.Player) (favOff r6.Operator, favDef r6.Operator) {
+	// Some intial operators to compare against
+	favOff = player.Operators["ash"]
+	favDef = player.Operators["frost"]
+
+	for _, op := range player.Operators {
+		if op.Role == "atk" {
+			if op.Playtime > favOff.Playtime {
+				favOff = op
+			}
+		}
+
+		if op.Role == "def" {
+			if op.Playtime > favDef.Playtime {
+				favDef = op
+			}
+		}
+	}
+
+	return
 }
 
 func newMsg(description string) *discordgo.MessageEmbed {
